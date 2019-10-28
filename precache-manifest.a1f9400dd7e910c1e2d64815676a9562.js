4b6183e00890ea1a786ce2eb7c4dad623220d035
GrinchakAndrew <andrewgrinchak@gmail.com>
2019-10-28 18:54:17 +0200

    fixes:3

---
+++ precache-manifest.a1f9400dd7e910c1e2d64815676a9562.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5afcc92f4c50a4fe0c444504b3dfb997",
+    "url": "/gapi-reworked/index.html"
+  },
+  {
+    "revision": "c20772c41aa55f7db17d",
+    "url": "/gapi-reworked/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0c1000a7170c3d7fc54c",
+    "url": "/gapi-reworked/static/js/2.709ea747.chunk.js"
+  },
+  {
+    "revision": "c20772c41aa55f7db17d",
+    "url": "/gapi-reworked/static/js/main.8ab3ac26.chunk.js"
+  },
+  {
+    "revision": "2a3354ef9be488d71389",
+    "url": "/gapi-reworked/static/js/runtime~main.20445510.js"
+  }
+]);
